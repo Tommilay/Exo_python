7972ffa312313e9153c62a3cfcf1296733cbd20f
Aina Tommy <nyonintsoatommy@gmail.com>
2025-01-15 10:23:11 +0300

    commit faharoa

--- exo/app/models.py
+++ exo/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    class Genre(models.TextChoices):
+        Romance = "R"
+        Fiction = "F"
+        Horreur = "H"
+        Fantastique = "Fa"
+        Dramatique = "D"
+    titre = models.fields.CharField(max_length=100)
+    auteur = models.fields.CharField(max_length=100)
+    date_d_edition = models.fields.DateField()
+    genre = models.fields.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.titre}"
+
+    
